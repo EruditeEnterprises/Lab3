d9d4262e6039fe47d286c243f0ec6ddab19e7f86
erexsolr <erexsolr@rose-hulman.edu>
2015-03-24 16:01:15 -0400

    Test 8 Satisfied

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -6,7 +6,7 @@ public class PrimeFactor {
 
 	public static List<Integer> primeFactors(int integer){
 		ArrayList<Integer> primes = new ArrayList<>();
-		if(integer % 2 == 0){
+		while(integer % 2 == 0){
 			primes.add(2);
 			integer/=2;
 		}
