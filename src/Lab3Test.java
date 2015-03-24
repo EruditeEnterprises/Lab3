d1862ecc5566e22176d05983abd1394ae5e98b9d
erexsolr <erexsolr@rose-hulman.edu>
2015-03-24 15:08:23 -0400

    Added First Test

---
+++ src/Lab3Test.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class Lab3Test {
+
+	@Test
+	public void testPrimeFactorsForOne() {
+		assertEquals(new ArrayList<Integer>(), primeFactors(1));
+	}
+
+	public ArrayList<Integer> primeFactors(int integer){
+		return null;
+	}
+}
