8ca695746bb6b220ad27c3f7abe59ac2caa5099c
erexsolr <erexsolr@rose-hulman.edu>
2015-03-24 21:09:36 -0400

    PrimeNumber: Satisfied Test for 5

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -7,6 +7,9 @@ public class PrimeNumber {
 	public static List<Integer> primesLessThan(int i){
 		ArrayList<Integer> primes = new ArrayList<>();
 		for(int j = 2;i>j; j++){
+			if((j % 2 == 0) && (j!=2)){
+				continue;
+			}
 			primes.add(j);
 		}
 		return primes;
